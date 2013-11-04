b3f1a1d52352905d6fd93143eab62d895aaae60d
Jason Shum <jason.ch.shum@gmail.com>
2013-11-03 23:03:33 -0500

    removed port from host

--- app/Config/database.php
+++ app/Config/database.php
@@ -13,7 +13,7 @@ class DATABASE_CONFIG {
 	public $default = array(
 		'datasource' => 'Database/Mysql',
 		'persistent' => false,
-      'host' => 'us-cdbr-east-04.cleardb.com:3306',
+      'host' => 'us-cdbr-east-04.cleardb.com',
 		'login' => 'be3a194532571b',
 		'password' => '6bda2634',
 		'database' => 'zanhealth',
