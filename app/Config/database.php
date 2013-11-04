c1f0bbff01f454052c300c504957958ea99018f0
mp42 <melpak92@yahoo.com>
2013-11-04 00:46:42 -0500

    fixed db settings for qa environment

--- app/Config/database.php
+++ app/Config/database.php
@@ -13,10 +13,10 @@ class DATABASE_CONFIG {
 	public $default = array(
 		'datasource' => 'Database/Mysql',
 		'persistent' => false,
-		'host' => 'localhost',
-		'login' => 'hceu',
-		'password' => '',
-		'database' => 'zanhealth',
+		'host' => 'us-cdbr-east-04.cleardb.com',
+		'login' => 'b3ec4cf5269650',
+		'password' => 'dc97910b',
+		'database' => 'heroku_06c59c668e56b06',
 		'encoding' => 'utf8'
 	);
 }
