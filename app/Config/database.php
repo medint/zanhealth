9dded09e2de48334bdd990691f6152644e6a277c
mp42 <melpak92@yahoo.com>
2013-11-03 23:32:34 -0500

    pushing changes to production

--- app/Config/database.php
+++ app/Config/database.php
@@ -13,10 +13,10 @@ class DATABASE_CONFIG {
 	public $default = array(
 		'datasource' => 'Database/Mysql',
 		'persistent' => false,
-		'host' => 'localhost',
-		'login' => 'hceu',
-		'password' => '',
-		'database' => 'zanhealth',
+		'host' => 'us-cdbr-east-04.cleardb.com/',
+		'login' => 'be3a194532571b',
+		'password' => '6bda2634',
+		'database' => 'heroku_782d3780fde94d7',
 		'encoding' => 'utf8'
 	);
 }
