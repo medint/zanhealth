e90dd2157364edcd50f28f0a37b1b0e97561aff4
mp42 <melpak92@yahoo.com>
2013-11-12 00:16:06 -0500

    added mysql syntax for foreign keys

--- create_db.sql
+++ create_db.sql
@@ -19,54 +19,15 @@ CREATE TABLE users (
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	username VARCHAR(50) NOT NULL,
 	password VARCHAR(60) NOT NULL,
-	role_id INT REFERENCES roles(id),
+    role_id INT,
 	created DATETIME,
-	modified DATETIME
+	modified DATETIME,
+    FOREIGN KEY (role_id) REFERENCES roles(id)
 );
 
-CREATE TABLE work_requests (
+CREATE TABLE facilities (
 	id INT PRIMARY KEY AUTO_INCREMENT,
-	date_requested DATETIME NOT NULL,
-	date_expire DATETIME,
-	date_completed DATETIME,
- 	request_type ENUM ('Breakdown','Preventative'),
- 	item INT REFERENCES items(id),
- 	cost INT NOT NULL,
- 	description TEXT,
- 	status ENUM ('Opened', 'Closed'),
- 	owner VARCHAR(45),
- 	requester_id INT REFERENCES users(id),
- 	cause_description TEXT,
- 	action_taken TEXT,
- 	prevention_taken TEXT,
- 	facility_comments TEXT,
- 	engineer_comments TEXT,
- 	manager_comments TEXT
-);
-
-CREATE TABLE items (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	domain ENUM ('U-MM','P-CC','P-PHL'),
-	tag INT NOT NULL,
-	name VARCHAR(255) NOT NULL,
-	model_id INT REFERENCES models(id),
-	serial_number VARCHAR(45),
-	year_manufactured INT,
-	funding VARCHAR (45),
-	date_received DATE,
-	warranty_expire DATE,
-	contract_expire DATE,
-	warranty_notes TEXT,
-	service_agent VARCHAR(45),
-	location_id INT REFERENCES location(id),
-	status ENUM ('Fully Functional',  'Not Functional', 'Needs Major Repair', 'Needs Minor Repair', 'Decommissioned', 'Disposed') NOT NULL,
-	utilization ENUM ('Normal', 'No Utilization', 'Very High', 'Very Low','Decomissioned', 'Disposed') NOT NULL,
-	remarks TEXT,
-	item_type ENUM('Biomedical'),
-	quantity INT,
-	item_spec VARCHAR(45),
-	price INT NOT NULL,
-	itemscol VARCHAR(45)
+	name VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE models (
@@ -75,31 +36,90 @@ CREATE TABLE models (
 	vendor_name VARCHAR(255) 
 );
 
+CREATE TABLE location  (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	floor INT NOT NULL,
+	building VARCHAR(50),
+    facilities_id INT,
+    FOREIGN KEY (facilities_id) REFERENCES facilities(id)
+);
+
 CREATE TABLE needs (
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	name VARCHAR(45) NOT NULL,
-	facility_id INT REFERENCES facility(id),
-	location_id INT REFERENCES location(id),
-	model_id INT REFERENCES models(id),
+    facility_id INT,
+    location_id INT,
+    model_id INT,
 	quantity INT NOT NULL,
 	urgency ENUM ('HIGH', 'MEDIUM','LOW'),
 	reason TEXT,
 	remarks TEXT,
 	stage ENUM ('OPEN', 'IN SHIPMENT','CLOSED'),
 	date_requested DATE,
-	user_id INT REFERENCES users(id)
+    user_id INT,
+    FOREIGN KEY (facility_id) REFERENCES facility(id),
+    FOREIGN KEY (location_id) REFERENCES location(id),
+    FOREIGN KEY (model_id) REFERENCES models(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
-CREATE TABLE facilities (
+CREATE TABLE items (
 	id INT PRIMARY KEY AUTO_INCREMENT,
-	name VARCHAR(255) NOT NULL
+	domain ENUM ('U-MM','P-CC','P-PHL'),
+	tag INT NOT NULL,
+	name VARCHAR(255) NOT NULL,
+    model_id INT,
+	serial_number VARCHAR(45),
+	year_manufactured INT,
+	funding VARCHAR (45),
+	date_received DATE,
+	warranty_expire DATE,
+	contract_expire DATE,
+	warranty_notes TEXT,
+	service_agent VARCHAR(45),
+    location_id INT,
+	status ENUM ('Fully Functional',  'Not Functional', 'Needs Major Repair', 'Needs Minor Repair', 'Decommissioned', 'Disposed') NOT NULL,
+	utilization ENUM ('Normal', 'No Utilization', 'Very High', 'Very Low','Decomissioned', 'Disposed') NOT NULL,
+	remarks TEXT,
+	item_type ENUM('Biomedical'),
+	quantity INT,
+	item_spec VARCHAR(45),
+	price INT NOT NULL,
+	itemscol VARCHAR(45),
+	FOREIGN KEY (model_id) REFERENCES models(id),
+    FOREIGN KEY (location_id) REFERENCES location(id)
 );
 
-CREATE TABLE location  (
+
+CREATE TABLE work_requests (
 	id INT PRIMARY KEY AUTO_INCREMENT,
-	floor INT NOT NULL,
-	building VARCHAR(50),
-	facilities_id INT REFERENCES facilities(id)
+	date_requested DATETIME NOT NULL,
+	date_expire DATETIME,
+	date_completed DATETIME,
+ 	request_type ENUM ('Breakdown','Preventative'),
+    item INT,
+ 	cost INT NOT NULL,
+ 	description TEXT,
+ 	status ENUM ('Opened', 'Closed'),
+ 	owner VARCHAR(45),
+    requester_id INT,
+ 	cause_description TEXT,
+ 	action_taken TEXT,
+ 	prevention_taken TEXT,
+ 	facility_comments TEXT,
+ 	engineer_comments TEXT,
+ 	manager_comments TEXT,
+    FOREIGN KEY (item) REFERENCES items(id),
+    FOREIGN KEY (requester_id) REFERENCES users(id)
 );
+
+
+
+
+
+
+
+
+
 
 
