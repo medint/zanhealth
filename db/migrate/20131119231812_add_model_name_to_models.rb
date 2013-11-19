223d3b3b4c48826c7a2135cff5b03a477681b129
William Tennien Murphy <will.t.murphy@gmail.com>
2013-11-19 18:19:09 -0500

    Added model_name to Models and room to Locations.

---
+++ db/migrate/20131119231812_add_model_name_to_models.rb
@@ -0,0 +1,4 @@
+class AddModelNameToModels < ActiveRecord::Migration
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131116035327) do
+ActiveRecord::Schema.define(version: 20131119231828) do
 
   create_table "facilities", force: true do |t|
     t.string   "name"
