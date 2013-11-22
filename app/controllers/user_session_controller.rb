20c82da3e6a25f6e3f95c0fc02179a15b7b12766
William Tennien Murphy <will.t.murphy@gmail.com>
2013-11-21 23:37:28 -0500

    Added authentication sketch to controller.

--- app/controllers/user_session_controller.rb
+++ app/controllers/user_session_controller.rb
@@ -1,6 +1,13 @@
 class UserSessionController < ApplicationController
-    def login
-        
+  def login
+    if session[:user].nil?
+        user = User.find(username: params[:username])
+        if not user.nil? and user.encrypted_password == params[:encrypted_password]
+            # authentication successful
+        else
+            # authentication failed
+            session[:user] = 
     end
-
+    # already logged in
+  end
 end
