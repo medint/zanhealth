3f7c0b014fe928604f5ade06c9fbd1ef2f01f055
William Tennien Murphy <will.t.murphy@gmail.com>
2013-11-25 03:00:43 -0500

    Config'd db.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.new(username: 'admin',
+         encrypted_password: '21232f297a57a5a743894a0e4a801fc3').save
