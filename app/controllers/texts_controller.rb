702476c56ce6f4e3d93f75c0e29ae51a94d44145
Tauseef Khan <tauseef_khan@brown.edu>
2013-12-26 01:39:09 +0500

    texts

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -16,6 +16,23 @@ class TextsController < ApplicationController
   def new
     @text = Text.new
   end
+  
+  def receive
+	message_body = params["Body"]
+    from_number = params["From"]
+	message = message_body.split("#")
+	if message.first == "item"
+		request = workRequest.create(:id => message_body.second)
+		workRequestComment.create(:work_request_id => request.id,:comment_text => message.third)
+	else
+		comment = workRequestComment.create(:work_request_id => message.second, :comment_text => message.fourth)
+		#if comment.work_request.status != message.third
+			#comment.work_request.status => message.third
+		#end
+	end
+    SMSLogger.log_text_message from_number, message_body
+  end
+end
 
   # GET /texts/1/edit
   def edit
