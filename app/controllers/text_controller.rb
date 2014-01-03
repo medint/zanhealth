d4455262b9378bc7f62a8a1c3c301b0bc0ba41c1
Tauseef Khan <tauseef_khan@brown.edu>
2014-01-04 02:31:14 +0500

    text support

--- app/controllers/text_controller.rb
+++ app/controllers/text_controller.rb
@@ -15,4 +15,21 @@ class TextController < ApplicationController
 	end
     SMSLogger.log_text_message from_number, message_body
   end
+  
+  def create
+	message_body = params["Body"]
+    from_number = params["From"]
+	puts "Message body is %s." %message_body
+	message = message_body.split("#")
+	if message.first == "item"
+		request = workRequest.create(:id => message_body.second)
+		workRequestComment.create(:work_request_id => request.id,:comment_text => message.third)
+	else
+		comment = workRequestComment.create(:work_request_id => message.second, :comment_text => message.fourth)
+		#if comment.work_request.status != message.third
+			#comment.work_request.status => message.third
+		#end
+	end
+    SMSLogger.log_text_message from_number, message_body
+  end
 end
