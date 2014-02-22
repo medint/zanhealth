ddde8169d7f99515f4b05ee48af46554eb881379
Sam Johnson <sljohnson135@gmail.com>
2014-02-22 18:13:33 -0500

    got to commit to push not sure what it is for, got some new ui i think

--- app/controllers/facility_work_requests_controller.rb
+++ app/controllers/facility_work_requests_controller.rb
@@ -1,5 +1,6 @@
 class FacilityWorkRequestsController < ApplicationController
-	
+before_action :set_facility_work_request, only: [:show, :update]
+
   layout 'layouts/facilities_app'
 
   def new
@@ -10,8 +11,28 @@ class FacilityWorkRequestsController < ApplicationController
   end
 
   def show
-      @wr_comment = WorkRequestComment.all
-      @labor_hours=LaborHour.all
+    @facility_work_requests = FacilityWorkRequest.all
+  end
+
+  def update
+    respond_to do |format|
+      if @facility_work_request.update(facility_work_request_params)
+        format.html { redirect_to @facility_work_request, notice: 'Work request was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @facility_work_request.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def set_facility_work_request
+    @facility_work_request = FacilityWorkRequest.find(params[:id])
+  end
+
+  def facility_work_request_params
+    p params
+    params.require(:facility_work_request).permit(:id, :requester, :department, :location, :phone, :email, :description, :created_at, :updated_at)
   end
 
 end
