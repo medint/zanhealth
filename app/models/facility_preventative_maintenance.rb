d990e4d417b1de28e6eddf3475d3f92acc7f2194
Jason Shum <jason@cs.brown.edu>
2014-03-02 06:11:39 +0800

    added days since

--- app/models/facility_preventative_maintenance.rb
+++ app/models/facility_preventative_maintenance.rb
@@ -1,5 +1,12 @@
 class FacilityPreventativeMaintenance < ActiveRecord::Base
   before_save :calc_next_date
+  attr_accessor :days_since
+
+  def calc_days_since
+    # unless self.next_date.nil?
+      self.days_since = (self.next_date - self.last_date_checked).to_i/1.day
+    #end
+  end
 
   private
     def calc_next_date
