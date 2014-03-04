e6ffc617a06c55186d75efc6de7b4b9d200c95f0
dav92lee <dav92lee@gmail.com>
2014-03-03 20:36:10 -0500

    dave's push for the week... fixed a lot of styling and started styling right side

--- app/assets/javascripts/facilities_app.js
+++ app/assets/javascripts/facilities_app.js
@@ -30,18 +30,23 @@ if (window.location.toString().match(/facility_work_orders/) || window.location.
 function window_resize_handler(){
 	var window_width = window.outerWidth;
 	var window_height = window.outerHeight;
-	console.log("HELLO!!!!!!");
 	var mid_summary = document.getElementById("mid-summary");
 	var right_detail = document.getElementById("right-detail");
+	var right_additional_info = document.getElementById("object_additional-details");
 
 	/* width */
-	mid_summary.style.width = ((window_width-240)/2)-5;
-	right_detail.style.width = ((window_width-240)/2)-5;
-	mid_summary.style.height = window_height-135;
-	right_detail.style.height = window_height-135;
+	mid_summary.style.width = ((window_width-240)/2);
+	right_detail.style.width = ((window_width-240)/2);
+	mid_summary.style.height = window_height-125;
+	right_detail.style.height = window_height-125;
 
 	$('.mid-summary_ul_li').css("width", (((window_width-240)/2)-1));
 	$('.mid-summary_ul_li_hr').css("width", (((window_width-240)/2)-1));
+
+	if (right_additional_info){ //check to see if right side is showing
+		right_additional_info.style.height=window_height-300-60;
+	}
+
 };
 
 function edit_button_handler(){
