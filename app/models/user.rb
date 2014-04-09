677a7d06f55b520768bdbe2500576850f1225ab7
Jason Shum <jason@cs.brown.edu>
2014-04-09 11:02:33 +0800

    changed login by username to by username or email

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,18 @@ class User < ActiveRecord::Base
   			:recoverable, :rememberable, :trackable, :validatable
 
 	belongs_to :facility
+	attr_accessor :login
 
+	validates :username,:uniqueness => {:case_sensitive => false }
   	validates :language, inclusion: { in: %w(english swahili, creole), message: 'For language, please select either "english" or "swahili"' }
+
+    def self.find_first_by_auth_conditions(warden_conditions)
+      conditions = warden_conditions.dup
+      if login = conditions.delete(:login)
+        where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+      else
+        where(conditions).first
+      end
+    end
 
 end
