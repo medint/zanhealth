100c5ea3efd7713864d679374dca4030efd3053a
lccodes <luke_camery@brown.edu>
2014-04-24 20:32:32 -0400

    Update recaptcha.rb
    Put in actual keys

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,5 +1,5 @@
 Recaptcha.configure do |config|
-  config.public_key  = '6LerAvISAAAAAF8RmVjgotdGtGcSKjVZkkruFU6v '
-  config.private_key = '6LerAvISAAAAAFHVqVwffmIr20nHVKSKEQ5Xac01 '
+  config.public_key  = '6LckpPASAAAAAFwt0Q13bS7N1uh_C1eAPL98EggN'
+  config.private_key = '6LckpPASAAAAALxvdXBMV_yV29xpUO7haIlyhjOq'
   config.proxy = 'http://zanhealth.co:8080'
 end
