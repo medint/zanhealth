dddccce7400fd7d5ef36a92ce8b054e6798aab86
Sam <sljohnson135@gmail.com>
2014-04-26 15:25:26 -0400

    facility dashbaord graphs and and some routes fixed

--- app/controllers/facility_dashboard_controller.rb
+++ app/controllers/facility_dashboard_controller.rb
@@ -11,17 +11,60 @@ class FacilityDashboardController < ApplicationController
 
 	def status
 		#invalid dates can be inputted
-		@work_orders=FacilityWorkOrder.where("date_expire >= :start_date AND date_expire <= :end_date", {start_date: @starting_date, end_date: @ending_date}).order(:status)
+
+		@work_orders=FacilityWorkOrder.joins({ :department => :facility}).where("date_expire >= :start_date AND date_expire <= :end_date AND facilities.id == :curruser", {start_date: @starting_date, end_date: @ending_date, curruser: current_user.facility_id}).order(:status)
 	end
 
 	def wo_finances
 
-		@work_orders = FacilityWorkOrder.where("date_completed >= :start_date AND date_completed <= :end_date AND status==2", {start_date: @starting_date, end_date: @ending_date}).order(:department_id)
-	
+		@work_orders = FacilityWorkOrder.joins({ :department => :facility}).where("date_completed >= :start_date AND date_completed <= :end_date AND status==2 AND facilities.id == :curruser", {start_date: @starting_date, end_date: @ending_date, curruser: current_user.facility_id}).order(:department_id)
+		@work_orders_json= {}
+		currdepart=0
+		costbydepart=0
+		@work_orders.each do |q|
+
+			if currdepart!=q.department
+				if currdepart!=0
+					@work_orders_json[currdepart.name]["totalcost"]=costbydepart
+				end
+				currdepart=q.department
+				@work_orders_json[q.department.name]={}
+				if q!=@work_orders.first
+					costbydepart=0					
+				end
+			end
+			
+			totalcost=0
+			q.facility_costs.each do |cost|
+				totalcost=totalcost+cost.cost*cost.unit_quantity
+			end
+			@work_orders_json[q.department.name][q.id]=totalcost
+			costbydepart+=totalcost		
+		end
+		@work_orders_json[currdepart.name]["totalcost"]=costbydepart
+
 	end
 
 	def labor_hours
 		@labor_hours = FacilityLaborHour.joins(:facility_work_order).where("date_completed >= :start_date AND date_completed <= :end_date AND status==2", {start_date: @starting_date, end_date: @ending_date}).order(:technician_id)
+		@labor_hours_json= {}
+		currtech=0
+		hoursbytech=0
+		@labor_hours.each do |q|
+			if currtech!=q.technician_id
+				if currtech!=0
+					@labor_hours_json[currtech.name]["totalcost"]=hoursbytech
+				end
+				currtech=q.technician
+				@labor_hours_json[q.technician.name]={}
+				if q!=@labor_hours.first
+					hoursbytech=0
+				end
+			end				
+			@labor_hours_json[q.technician.name][q.id]=q.duration	
+			hoursbytech+=q.duration			
+		end
+		@labor_hours_json[currtech.name]["totalcost"]=hoursbytech
 	end
 
 	def set_status
