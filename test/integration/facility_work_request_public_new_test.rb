0a84d6020f53a1867fb9b977e10eb858df18c621
Jason Shum <jason.ch.shum@gmail.com>
2014-05-10 23:25:40 -0400

    test for external creation of work requests, recaptcha seems to be passing without workarounds, might be because of javascript shutdown by phantomjs [#71083862]

--- test/integration/facility_work_request_public_new_test.rb
+++ test/integration/facility_work_request_public_new_test.rb
@@ -1,19 +1,29 @@
 require 'test_helper'
 Capybara.current_driver = :poltergeist
 
-class FacilityWorkRequestNewTest < ActionDispatch::IntegrationTest
+class FacilityWorkRequestPublicNewTest < ActionDispatch::IntegrationTest
   setup do  	
 		
   end
 
   test "create new work request form" do
-      visit '/facility_work_requests/1/new' 
+    visit '/facility_work_requests/1/public_new' 
 
-		assert_equal '/facility_work_requests/1/new', current_path
+		assert_equal '/facility_work_requests/1/public_new', current_path
+
+    fill_in 'facility_work_request[requester]', :with => 'John'
+    fill_in 'facility_work_request[department]', :with => 'John'
+    fill_in 'facility_work_request[location]', :with => 'John'
+    fill_in 'facility_work_request[phone]', :with => 'John'
+    fill_in 'facility_work_request[email]', :with => 'John'
+    fill_in 'facility_work_request[description]', :with => 'John'
 
     click_button 'Create'
-    # important note, if javascript not enabled, then will allow creation
-    # unless backend validation is there.
 
+    current_url = current_path.split('/') #facility_work_requests/public_show/:id
+    assert_equal 'facility_work_requests', current_url[1]
+    assert_equal 'public_show', current_url[2]    
   end
 end
+
+
