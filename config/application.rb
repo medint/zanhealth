b48e38957d3fa4e33465e408222cd6aeb9d707f6
Sam <sljohnson135@gmail.com>
2014-05-11 17:51:07 -0400

    trying to fix printing

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module Zanhealth
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    config.assets.precompile += ['welcome.js', 'facilities_app.js']
+    config.assets.precompile += ['welcome.js', 'facilities_app.js', 'application-print-main-list.css']
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
