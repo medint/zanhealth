ea2b04a8fb29b364c660d7642e615f00df295d31
mp42 <melpak92@yahoo.com>
2014-05-28 00:36:17 -0700

    added authorization for parts and parts transactions

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -30,30 +30,30 @@ class Ability
     # https://github.com/ryanb/cancan/wiki/Defining-Abilities
       if user.role.name == "bmet_tech"
        	  can :manage, [BmetCost, BmetItemHistory, BmetLaborHour, BmetNeed, BmetWorkOrderComment]
-          can [:read, :create, :update], [BmetWorkOrder,BmetWorkRequest,BmetItem,BmetModel]
+          can [:read, :create, :update], [BmetWorkOrder,BmetWorkRequest,BmetItem,BmetModel,Part,PartTransaction]
           can [:hidden, :all, :show_all, :show_hidden, :set_users, :set_departments, :set_status,:as_csv,:search], [BmetWorkOrder, BmetWorkRequest,BmetPreventativeMaintenance]
           can [:set_hidden_bmet_work_orders, :set_all_bmet_work_orders, :set_bmet_work_order], BmetWorkOrder 
           can [:set_hidden_bmet_work_requests, :set_all_bmet_work_requests], BmetWorkRequest
           can [:read, :create, :set_hidden_bmet_preventative_maintenance, :set_all_bmet_preventative_maintenance], BmetPreventativeMaintenance
-          cannot [:delete,:hide], [BmetItem, BmetWorkOrder,BmetWorkRequest,BmetPreventativeMaintenance, BmetModel] 
+          cannot [:delete,:hide], [BmetItem, BmetWorkOrder,BmetWorkRequest,BmetPreventativeMaintenance, BmetModel, Part,PartTransaction] 
 	  elsif user.role.name == "fac_tech"
-	  	  can :manage, [FacilityCost, FacilityLaborHour, FacilityWorkOrderComment, FacilityDashboard]
-	  	  can [:read, :create, :update], [FacilityWorkOrder, FacilityWorkRequest]
+	  	  can :manage, [FacilityCost, FacilityLaborHour, FacilityWorkOrderComment]
+	  	  can [:read, :create, :update], [FacilityWorkOrder, FacilityWorkRequest,Part,PartTransaction]
 	  	  can [:hidden, :all, :show_all, :show_hidden, :set_users, :set_departments, :set_status,:as_csv,:search], [FacilityWorkOrder,FacilityWorkRequest, FacilityPreventativeMaintenance]
 	  	  can [:set_hidden_facility_work_orders, :set_all_facility_work_orders], FacilityWorkOrder
 	  	  can [:set_hidden_facility_work_requests, :set_all_facility_work_requests], FacilityWorkRequest
 	  	  can [:read, :create, :set_hidden_facility_preventative_maintenance, :set_all_facility_preventative_maintenance], FacilityPreventativeMaintenance
-	  	  cannot [:destroy, :hide], [FacilityWorkOrder,FacilityWorkRequest,FacilityPreventativeMaintenance]
+	  	  cannot [:destroy, :hide], [FacilityWorkOrder,FacilityWorkRequest,FacilityPreventativeMaintenance,Part,PartTransaction]
 	  elsif user.role.name == "bmet_fac_tech"
-	  	  can :manage, [FacilityCost, FacilityLaborHour, FacilityWorkOrderComment, BmetCost, BmetLaborHour, BmetWorkOrderComment,FacilityDashboard]
-	  	  can [:read, :create, :update], [BmetWorkOrder, FacilityWorkOrder,BmetWorkRequest, FacilityWorkRequest,BmetItem,BmetModel]
+	  	  can :manage, [FacilityCost, FacilityLaborHour, FacilityWorkOrderComment, BmetCost, BmetLaborHour, BmetWorkOrderComment]
+	  	  can [:read, :create, :update], [BmetWorkOrder, FacilityWorkOrder,BmetWorkRequest, FacilityWorkRequest,BmetItem,BmetModel,Part,PartTransaction]
 	  	  can [:hidden, :all, :show_all, :show_hidden, :set_users, :set_departments, :set_status, :as_csv, :search], [BmetWorkOrder, FacilityWorkOrder,BmetWorkRequest, FacilityWorkRequest, BmetPreventativeMaintenance, FacilityPreventativeMaintenance]
 	  	  can [:set_hidden_facility_work_orders, :set_all_facility_work_orders], FacilityWorkOrder
        	  can [:set_hidden_bmet_work_orders, :set_all_bmet_work_orders], BmetWorkOrder 
        	  can [:set_hidden_facility_work_requests, :set_all_facility_work_requests], FacilityWorkRequest
        	  can [:set_hidden_bmet_work_requests, :set_all_bmet_work_requests], BmetWorkRequest
        	  can [:read, :create, :set_hidden_bmet_preventative_maintenance, :set_all_bmet_preventative_maintenance, :set_hidden_facility_preventative_maintenance, :set_all_facility_preventative_maintenance], [BmetPreventativeMaintenance, FacilityPreventativeMaintenance]
-	  	  cannot [:destroy, :hide], [BmetWorkOrder, FacilityWorkOrder, BmetWorkRequest, FacilityWorkRequest, BmetPreventativeMaintenance, FacilityPreventativeMaintenance,BmetItem, BmetModel]
+	  	  cannot [:destroy, :hide], [BmetWorkOrder, FacilityWorkOrder, BmetWorkRequest, FacilityWorkRequest, BmetPreventativeMaintenance, FacilityPreventativeMaintenance,BmetItem, BmetModel,Part, PartTransaction]
 	  end
   end
 end
