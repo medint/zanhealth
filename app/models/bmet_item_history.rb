1b3de39a15575d3519784bcdec3eff48cf7860ae
jshum <jason.ch.shum@gmail.com>
2014-06-05 01:14:18 +0300

    significant layout changes to bmet_items, added status,condition,work_order_status to bmet_item_history and created the triggers, added bmet_items to bmet_work_orders/new|edit

--- app/models/bmet_item_history.rb
+++ app/models/bmet_item_history.rb
@@ -2,16 +2,18 @@
 #
 # Table name: bmet_item_histories
 #
-#  id           :integer          not null, primary key
-#  bmet_item_id :integer
-#  datetime     :datetime
-#  status       :integer
-#  utilization  :integer
-#  remarks      :text
-#  created_at   :datetime
-#  updated_at   :datetime
+#  id                  :integer          not null, primary key
+#  bmet_item_id        :integer
+#  bmet_item_status    :integer
+#  remarks             :text
+#  created_at          :datetime
+#  updated_at          :datetime
+#  work_order_id       :integer
+#  bmet_item_condition :integer
+#  work_order_status   :integer
 #
 
 class BmetItemHistory < ActiveRecord::Base
     belongs_to :bmet_item
+    belongs_to :work_order, :class_name => "BmetWorkOrder"
 end
--- app/views/bmet_item_histories/index.json.jbuilder
+++ app/views/bmet_item_histories/index.json.jbuilder
@@ -1,4 +1,4 @@
-json.array!(@item_histories) do |item_history|
-  json.extract! item_history, :bmet_item_id, :datetime, :status, :utilization, :remarks
-  json.url item_history_url(item_history, format: :json)
+json.array!(@bmet_item_histories) do |bmet_item_history|
+  json.extract! bmet_item_history, :id, :bmet_item_id, :bmet_item_status, :bmet_item_condition, :remarks, :work_order, :work_order_status
+  json.url bmet_item_history_url(bmet_item_history, format: :json)
 end
