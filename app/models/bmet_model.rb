d7aba3a62018d87508d61f4c8b9cc18daa328da0
Alan Hwang <alan_hwang@brown.edu>
2014-06-05 22:00:31 +0300

    Import logic working as intended (items grab by asset_id to allow editing fields without importing entirely new items)

--- app/models/bmet_model.rb
+++ app/models/bmet_model.rb
@@ -18,13 +18,16 @@ class BmetModel < ActiveRecord::Base
   has_many :bmet_items
 
   def name
-    "#{manufacturer_name} #{category} #{model_name}" 
+    "#{manufacturer_name} #{category} #{model_name}"
   end
 
   def self.import(file, facility_id)
     CSV.foreach(file.path, headers: true) do |row|
         match = BmetModel.find_by_model_name(row["model_name"])
-        if !match || !BmetModel.where("match.bmet_items.collection(force_reload=false)[0].department.facility_id = ?", facility_id)
+        if !match ||
+        match.manufacturer_name != row["model_name"] ||
+        match.vendor_name != row["vendor_name"] ||
+        !BmetModel.where("match.bmet_items.collection(force_reload=false)[0].department.facility_id = ?", facility_id)
           mod = BmetModel.new
           mod.model_name = row["model_name"]
           mod.manufacturer_name = row["manufacturer_name"]
