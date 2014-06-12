649b30d7bdc6560dcd7606971e3fdcb0d951abe9
jshum <jason.ch.shum@gmail.com>
2014-06-12 18:03:26 +0300

    added /admin page and changed the abilities so only 'admin' role can access

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,5 @@
 class AdminController < ApplicationController
-	
+    authorize_resource :class => false
 	layout 'layouts/bmet_app'
 
 	def index
@@ -13,6 +13,14 @@ class AdminController < ApplicationController
     def generate_tags_pdf 
         pdf = TagPdf.new(params[:print_tags_form][:start_num],params[:print_tags_form][:end_num],params[:print_tags_form][:asset_id_prefix])
         send_data pdf.render, filename: 'item_tags.pdf', type: 'application/pdf'
+    end
+
+    def facilities
+        @facilities = Facility.all
+    end
+
+    def facility_users
+        @users = User.where(:facility_id => params[:facility_id])
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -156,6 +156,8 @@ Zanhealth::Application.routes.draw do
   get "/admin", to: "admin#index"
   get "/admin/print_tags_form", to: "admin#print_tags_form"
   get "/admin/generate_tags_pdf", to: "admin#generate_tags_pdf"
+  get "admin/facilities", to: "admin#facilities"
+  get "admin/facility_users/:facility_id", to: "admin#facility_users"
 
   get '/404', :to => redirect('/404.html')
   root to: "application#home"
