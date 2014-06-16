b29cf4c80723275fe0871d44499c0d40c2cdb8ab
mp42 <melpak92@yahoo.com>
2014-06-15 21:14:13 -0700

    redirect to bmet work order on login

--- config/routes.rb
+++ config/routes.rb
@@ -161,7 +161,7 @@ Zanhealth::Application.routes.draw do
   get "admin/facility_users/:facility_id", to: "admin#facility_users"
 
   get '/404', :to => redirect('/404.html')
-  root to: "application#home"
+  root to: "bmet_work_orders#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
