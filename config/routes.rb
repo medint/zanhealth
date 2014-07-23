3a735ac760303de2ab3ca6b4d9bdc6133c677397
Sam <sljohnson135@gmail.com>
2014-07-23 15:36:08 -0400

    added 37military public route

--- config/routes.rb
+++ config/routes.rb
@@ -195,8 +195,10 @@ Zanhealth::Application.routes.draw do
   root to: "bmet_work_orders#index"
 
   # Facility specific urls
+  get '/37military_work_requests', :to => redirect('/bmet_work_requests/4/public_new')
   get '/phm_work_requests', :to => redirect('/bmet_work_requests/3/public_new')
   get '/tmj_work_requests', :to => redirect('/bmet_work_requests/2/public_new')
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
