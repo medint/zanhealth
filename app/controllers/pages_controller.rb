ef40e880a259031286bc3a376425b5f136c3941e
Alan Hwang <alan_hwang@brown.edu>
2014-08-21 22:01:31 -0400

    Modify routing behavior for landing page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,9 @@ class PagesController < ApplicationController
     skip_before_action :authenticate_user!, only: [:home]
 
     def home
-
+    	if current_user
+    		redirect_to bmet_work_orders_path
+    	end
     end
 	
 end
